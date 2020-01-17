e36ff29a68ff20b5986ae60e35ab450ae0a3b230
Roy Plu <roy.plu@cegeka.com>
2020-01-17 15:22:33 +0100

    US5

--- Battle.Tests/ArmyTests.cs
+++ Battle.Tests/ArmyTests.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq;
+using Battle.Weapons;
 using FluentAssertions;
 using Xunit;
 
@@ -6,10 +7,25 @@ namespace Battle.Tests
 {
     public class ArmyTests
     {
+        private Soldier _gimli = new Soldier("Gimli", new Axe());
+        private Soldier _aragorn = new Soldier("Aragorn", new Sword());
+        private Soldier _angmar = new Soldier("Angmar", new Axe());
+        private Soldier _orc = new Soldier("Orc", new Spear());
+
+        [Fact]
+        public void CreateArmy_GivenAName_ArmyIsCreatedWithGivenName()
+        {
+            const string armyName = "FellowshipOfTheRing";
+
+            var army = new Army(armyName);
+
+            army.Name.Should().Be(armyName);
+        }
+
         [Fact]
         public void CreateArmy_ArmyCreatedWithNoEnlistedSoldiers()
         {
-            var army = new Army();
+            var army = new Army("FellowshipOfTheRing");
 
             army.EnlistedSoldiers.Should().BeEmpty();
         }
@@ -17,7 +33,7 @@ namespace Battle.Tests
         [Fact]
         public void Enlist_GivenASoldier_ThanSoldierIsEnlisted()
         {
-            var army = new Army();
+            var army = new Army("FellowshipOfTheRing");
             var soldierToEnlist = new Soldier("Gimli");
 
             army.Enlist(soldierToEnlist);
@@ -29,7 +45,7 @@ namespace Battle.Tests
         [Fact]
         public void GetFrontMan_Given2EnlistedSoldiers_ThanFirstEnlistedSoldierIsFrontMan()
         {
-            var army = new Army();
+            var army = new Army("FellowshipOfTheRing");
             var firstEnlistedSoldier = new Soldier("Gimli");
             var secondEnlistedSoldier = new Soldier("Aragorn");
 
@@ -38,6 +54,60 @@ namespace Battle.Tests
 
             army.EnlistedSoldiers.Should().HaveCount(2);
             army.FrontMan.Should().Be(firstEnlistedSoldier);
+        }
+
+        [Fact]
+        public void Engage_GivenTwoArmies_TheyCanFightEachOther()
+        {
+            var fellowship = CreateFellowshipOfTheRing(_aragorn,_gimli);
+            var armyOfMordor = CreateArmyOfMordor(_angmar,_orc);
+
+            var outcome = fellowship.Engage(armyOfMordor);
+            outcome.Should().NotBeNull();
+        }
+
+        [Fact]
+        public void Engage_GivenAttackingArmyHasTheStrongestWeapons_ArmiesOfEqualSize_ThenReturnsNameAttackingArmy()
+        {
+            var fellowship = CreateFellowshipOfTheRing(_aragorn,_gimli);
+            var armyOfMordor = CreateArmyOfMordor(_angmar,_orc);
+
+            var outcome = fellowship.Engage(armyOfMordor);
+            outcome.Should().Be(fellowship.Name);
+        }
+
+        [Fact]
+        public void Engage_GivenDefendingArmyHasTheStrongestWeapons_ArmiesOfEqualSize_ThenReturnsNameDefendingArmy()
+        {
+            var fellowship = CreateFellowshipOfTheRing(_aragorn);
+            var armyOfMordor = CreateArmyOfMordor(_angmar);
+
+            var outcome = fellowship.Engage(armyOfMordor);
+            outcome.Should().Be(armyOfMordor.Name);
+        }
+
+        private Army CreateArmyOfMordor(params Soldier[] soldiers)
+        {
+            var army = new Army("ArmyOfMordor");
+
+            foreach (var soldier in soldiers)
+            {
+                army.Enlist(soldier);
+            }
+
+            return army;
+        }
+
+        private  Army CreateFellowshipOfTheRing(params Soldier[] soldiers)
+        {
+            var army = new Army("FellowshipOfTheRing");
+
+            foreach (var soldier in soldiers)
+            {
+                army.Enlist(soldier);
+            }
+
+            return army;
         }
     }
 }
--- Battle/Army.cs
+++ Battle/Army.cs
@@ -6,8 +6,14 @@ namespace Battle
 {
     public class Army
     {
+        public Army(string name)
+        {
+            Name = name;
+        }
+
         public List<Soldier> EnlistedSoldiers { get; } = new List<Soldier>();
         public Soldier FrontMan { get; private set; }
+        public string Name { get; }
 
         public void Enlist(Soldier soldierToEnlist)
         {
@@ -22,6 +28,61 @@ namespace Battle
         private bool IsFirstEnlistedSoldier()
         {
             return !EnlistedSoldiers.Any();
+        }
+
+        public string Engage(Army defendingArmy)
+        {
+            var victorer = string.Empty;
+            var warIsOver = false;
+
+            while (!warIsOver)
+            {
+                ResolveFight(defendingArmy);
+
+                if (AttackingArmyHasWon(defendingArmy))
+                {
+                    warIsOver = true;
+                    victorer = Name;
+                }
+
+                if (DefendingArmyHasWon(defendingArmy))
+                {
+                    warIsOver = true;
+                    victorer = defendingArmy.Name;
+                }
+            }
+
+            return victorer;
+        }
+
+        private bool AttackingArmyHasWon(Army defendingArmy)
+        {
+            return FrontMan != null && defendingArmy.FrontMan == null;
+        }
+
+        private bool DefendingArmyHasWon(Army defendingArmy)
+        {
+            return FrontMan == null && defendingArmy.FrontMan != null;
+        }
+
+        private void ResolveFight(Army defendingArmy)
+        {
+            var outcome = FrontMan.Fight(defendingArmy.FrontMan);
+
+            if (outcome == FrontMan.Name)
+            {
+                defendingArmy.KillFrontMan();
+            }
+            else
+            {
+                KillFrontMan();
+            }
+        }
+
+        private void KillFrontMan()
+        {
+            EnlistedSoldiers.Remove(FrontMan);
+            FrontMan = EnlistedSoldiers.FirstOrDefault();
         }
     }
 }
